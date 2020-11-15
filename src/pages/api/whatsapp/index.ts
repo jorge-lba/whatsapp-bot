c1000e7e2a52b4dc4ff29ef593d932fa2fff0cc2
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-11-15 15:32:35 -0300

    Add - Ajustes na respostas e envio de msg para a pessoa que solicitou

--- src/pages/api/whatsapp/index.ts
+++ src/pages/api/whatsapp/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import axios, { AxiosInstance } from 'axios'
 
 import mentoringData from '../../../utils/mentoring-data'
+import { Mentoring } from '../../../interfaces'
 
 async function handler (_req: NextApiRequest, res: NextApiResponse) {
   const method = _req.method
@@ -9,9 +10,38 @@ async function handler (_req: NextApiRequest, res: NextApiResponse) {
   if(method === 'POST') await message(_req, res)
 }
 
-const ZENVIA_SANDBOX_TOKEN = `${process.env.ZENVIA_SANDBOX_TOKEN}`
-const ZENVIA_WHATSAPP_URL = `${process.env.ZENVIA_WHATSAPP_URL}`
+async function message(_req: NextApiRequest, res: NextApiResponse){
+  try {
+    const ZENVIA_SANDBOX_TOKEN = `${process.env.ZENVIA_SANDBOX_TOKEN}`
+    const ZENVIA_WHATSAPP_URL = `${process.env.ZENVIA_WHATSAPP_URL}`
 
+    const provider = configureProvider(ZENVIA_SANDBOX_TOKEN, ZENVIA_WHATSAPP_URL)
+
+    const from = _req.body.message.to
+    const to = _req.body.message.from
+
+    console.log(from, to)
+
+    const message = _req.body.message.contents[0].text.trim().split(' ')
+    const [command, complement]:[string, string] = message[0].split('#')
+
+    await testCommandIsValid(command)
+      .then(() => mentoringIsValid(parseInt(complement)))
+      .then(mentoring => senderIsMentorOrParticipant(to)(mentoring))
+      .then(whatsappNumbers => whatsappNumbers.map( ({to, message}) => formatContactMessageText(from, to, message)))
+      .then(contacts => contacts.map(sendingMessage(provider)))
+      .then(() => formatContactMessageText(from, to, 'Enviamos um aviso para os interessados.'))
+      .then(sendingMessage(provider))
+      .catch(err => console.log(err))
+
+    res.status(200).json({
+      message: 'Sua mensagem foi enviada'
+    })  
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, message: error.message })
+  }
+  
+}
  
 type BodySengingText = {
   from: string,
@@ -28,8 +58,8 @@ const commandList = [
   'mentoria'
 ]
 
-async function configureProvider(token:string, baseURL:string){
-  return await axios.create({
+function configureProvider(token:string, baseURL:string){
+  return axios.create({
     baseURL, 
     headers: {
       'content-type': 'application/json',
@@ -38,17 +68,22 @@ async function configureProvider(token:string, baseURL:string){
   })
 }
 
-async function sendingMessage(request: AxiosInstance,contact:BodySengingText){
-    return await request({
-      method: 'POST',
-      data: JSON.stringify(contact)
-    })
+function sendingMessage(request: AxiosInstance){
+    return async function (contact:BodySengingText){
+      console.log(contact)
+      await request({
+        method: 'POST',
+        data: JSON.stringify(contact)
+      })
+    }
 }
 
 async function mentoringIsValid(id:number){
   const mentoring = await mentoringData[0]
-  console.log(mentoring.id, id)
-  return mentoring.id === id ? true : false
+  if(mentoring.id === id){
+    return mentoring
+  }
+  throw 'Mentoria inválida'
 }
 
 function testContentArray<T>(array:T[]) {
@@ -61,42 +96,38 @@ function testContentArray<T>(array:T[]) {
   }
 }
 
-const testCommandIsValid = testContentArray<string>(commandList)
-
-async function message(_req: NextApiRequest, res: NextApiResponse){
-  try {
-
-    const contact:BodySengingText = {
-      from: _req.body.message.to,
-      to: _req.body.message.from,
-      contents:[{ 
-        type: 'text',
-        text: _req.body.message.contents[0].text
-      }, ]
+function senderIsMentorOrParticipant(from:string){
+  return function(mentoring:Mentoring){
+    if(mentoring.mentor.whatsapp === from){
+      return mentoring.team.participants.map(participant => ({
+        to:participant.whatsapp,
+        message: `O mentor(a) ${mentoring.mentor.name} está esperando o seu time para a mentoria.`
+      }))
+    }
+    
+    const participant = mentoring.team.participants.find(participant => participant.whatsapp === from)
+    if(participant){
+      return [{
+        to: mentoring.mentor.whatsapp,
+        message: `Boa tarde, o time ${mentoring.team.id} está esperando seu contato para a mentoria.`  
+      }]
     }
 
-    const message = _req.body.message.contents[0].text.trim().split(' ')
-    const [command, complement]:[string, string] = message[0].split('#')
-
-    const test = await testCommandIsValid(command)
-      .then(() => mentoringIsValid(parseInt(complement)))
-      .then(console.log)
-      .catch(err => console.log(err))
-
-    console.log(test)
-
-    const requestResult = await configureProvider(ZENVIA_SANDBOX_TOKEN, ZENVIA_WHATSAPP_URL)
-      .then(provider => sendingMessage(provider, contact))
-
-    console.log(requestResult.data)
-
-    res.status(200).json({
-      message: 'Sua mensagem foi enviada'
-    })  
-  } catch (error) {
-    res.status(500).json({ statusCode: 500, message: error.message })
+    throw 'Você não faz parte dessa mentoria, verifique o ID da sua mentoria'
   }
-  
 }
+
+function formatContactMessageText(from: string, to: string, message: string): BodySengingText{
+  return {
+    from,
+    to,
+    contents:[{ 
+      type: 'text',
+      text: message
+    }, ]
+  }
+}
+
+const testCommandIsValid = testContentArray<string>(commandList)
 
 export default handler
