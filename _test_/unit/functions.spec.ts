bc30a327767b3376fa87a3bd7064012cb27319f1
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-11-17 23:12:20 -0300

    Add - Teste da funçao que formata varios contatos

--- _test_/unit/functions.spec.ts
+++ _test_/unit/functions.spec.ts
@@ -6,12 +6,12 @@ import {
   testAnItemAgaintsVariousData,
   senderIsMentorOrParticipant,
   formatContactMessageText,
-  // formatMultContactsMessageText,
+  formatMultContactsMessageText
 }from '../../src/pages/api/whatsapp/functions'
 
 import mentoringData from '../../src/utils/mentoring-data-test'
 
-config({path: '.env.local'})
+config({path: '.env.test.local'})
 
 test('Deve dividir uma mensagem recebida em commando, complemento e texto', () => {
   const inputValue = 'mentoria#1 Estou esperando vocês para a mentoria.'
@@ -44,16 +44,17 @@ test('Deve retornar um item em um array de objetos', () => {
 test('Deve verificar se o input corresponde a um participante ou mentor', () => {
   const mentoring = mentoringData[0]
   const inputValue = `${process.env.WHATSAPP_PARTICIPANT}`
+  const valueTo = `${process.env.WHATSAPP_MENTOR}`
 
   const [result] = senderIsMentorOrParticipant(inputValue)(mentoring)
 
-  expect(result.to).toEqual(inputValue)
-  expect(result.message).toEqual('O mentor(a) Jane está esperando o seu time para a mentoria.')
+  expect(result.to).toEqual(valueTo)
+  expect(result.message).toEqual('Boa tarde, o time 1 está esperando seu contato para a mentoria.')
 })
 
 test('Deve formatar os dados de entrada e transformar em um objeto', () => {
-  const inputFrom = `${process.env.WHATSAPP_PARTICIPANT}`
-  const inputTo = `${process.env.ZENVIA_FROM}`
+  const inputTo = `${process.env.WHATSAPP_PARTICIPANT}`
+  const inputFrom = `${process.env.ZENVIA_FROM}`
   const inputMessage = 'OK'
 
   const result = formatContactMessageText(inputFrom, inputTo, inputMessage)
@@ -61,4 +62,19 @@ test('Deve formatar os dados de entrada e transformar em um objeto', () => {
   expect(result.from).toEqual(inputFrom)
   expect(result.to).toEqual(inputTo)
   expect(result.contents[0].text).toEqual(inputMessage)
+})
+
+test('Deve formatar vários dados de entrada e transformar em um objeto', () => {
+  const mentoring = mentoringData[0]
+  const inputValue = `${process.env.WHATSAPP_MENTOR}`
+  const inputFrom = `${process.env.ZENVIA_FROM}`
+  const valueTo = `${process.env.WHATSAPP_PARTICIPANT}`
+
+  const list = senderIsMentorOrParticipant(inputValue)(mentoring)
+
+  const result = formatMultContactsMessageText(inputFrom)(list)
+
+  expect(result[0].from).toEqual(inputFrom)
+  expect(result[0].to).toEqual(valueTo)
+  expect(result[0].contents[0].text).toEqual('O mentor(a) Jane está esperando o seu time para a mentoria.')
 })
