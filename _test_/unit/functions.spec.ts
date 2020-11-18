321630d4960b5ab4a97d10bdfc8dc289f40cfee4
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-11-17 22:17:24 -0300

    Add - Teste da funçao que seleciona um objeto dentro de um array

--- _test_/unit/functions.spec.ts
+++ _test_/unit/functions.spec.ts
@@ -1,11 +1,13 @@
 import {
   splitAndFormatMessage,
   testContentArray,
+  testAnItemAgaintsVariousData,
   // senderIsMentorOrParticipant,
   // formatContactMessageText,
   // formatMultContactsMessageText,
-  // testAnItemAgaintsVariousData
 }from '../../src/pages/api/whatsapp/functions'
+
+import mentoringData from '../../src/utils/mentoring-data'
 
 test('Deve dividir uma mensagem recebida em commando, complemento e texto', () => {
   const inputValue = 'mentoria#1 Estou esperando vocês para a mentoria.'
@@ -23,4 +25,14 @@ test('Deve verificar se um valor de entrada se encontra em um lista, se sim reto
   const result = await testContentArray<string>(list)(inputValue)
 
   expect(result).toEqual(inputValue)
+})
+
+test('Deve retornar um item em um array de objetos', () => {
+  const list = mentoringData
+  const item = 'id'
+  const inputValue = 1
+
+  const result = testAnItemAgaintsVariousData(list)(item)(inputValue)
+
+  expect(result).toEqual(list[0])
 })
