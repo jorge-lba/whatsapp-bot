86f55f211e3df7fc545809aeb3f9bcefd3bfa9f3
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-11-17 21:43:26 -0300

    Add - Teste da funçao que quebra a mensagem enviada

--- _test_/unit/functions.spec.ts
+++ _test_/unit/functions.spec.ts
@@ -1,3 +1,17 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+import {
+  splitAndFormatMessage,
+  // senderIsMentorOrParticipant,
+  // formatContactMessageText,
+  // testContentArray,
+  // formatMultContactsMessageText,
+  // testAnItemAgaintsVariousData
+}from '../../src/pages/api/whatsapp/functions'
+
+test('Deve dividir uma mensagem recebida em commando, complemento e texto', () => {
+  const inputValue = 'mentoria#1 Estou esperando vocês para a mentoria.'
+  const {command, complement, text} = splitAndFormatMessage(inputValue)
+  
+  expect(command).toEqual('mentoria')
+  expect(complement).toEqual('1')
+  expect(text).toEqual('Estou esperando vocês para a mentoria.')
 })
