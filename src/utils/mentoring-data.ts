26bab7523f37879e651cc98efcc1d7fd87058ce9
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-11-20 12:45:24 -0300

    Add - Mais duas mentorias para teste

--- src/utils/mentoring-data.ts
+++ src/utils/mentoring-data.ts
@@ -25,6 +25,44 @@ const mentoring:Mentoring[] = [{
       }
     ]
   }
+},
+{
+  id: 2,
+  date: new Date(),
+  mentor:{
+    id: 1,
+    name: 'Rahmai',
+    whatsapp:`${process.env.WHATSAPP_RAHMAI}`
+  },
+  team:{
+    id: 1,
+    participants:[
+      {
+        id: 1,
+        name: 'Giuliano',
+        whatsapp: `${process.env.WHATSAPP_GIU}`
+      }
+    ]
+  }
+},
+{
+  id: 3,
+  date: new Date(),
+  mentor:{
+    id: 1,
+    name: 'Giuliano',
+    whatsapp: `${process.env.WHATSAPP_GIU}`
+  },
+  team:{
+    id: 1,
+    participants:[
+      {
+        id: 1,
+        name: 'Rahmai',
+        whatsapp:`${process.env.WHATSAPP_RAHMAI}`
+      }
+    ]
+  }
 }] 
 
 export default mentoring
